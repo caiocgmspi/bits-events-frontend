0d8e0c4ea58365016190afbb4fb0161bf9c076d0
italo <italoguilherme900@gmail.com>
2024-05-05 20:28:25 -0300

    adicionado botao criar evento

--- src/components/pages/events/ListEvents.vue
+++ src/components/pages/events/ListEvents.vue
@@ -1,15 +1,20 @@
+<template>
+    <PageComponent title="Eventos">
+        <div class="actions">
+            <RouterLink :to="'/events/create'" class="btn btn-primary" style="float: right; margin-top: 50px;">
+                Criar Evento
+            </RouterLink>
+        </div>
+
+        <CardItem v-for="item in load()" :key="item?.id" :item="item" relation="events"/>
+    </PageComponent>
+</template>
+
 <script setup>
 import CardItem from '@/components/cardItem.vue';
 import PageComponent from '@/components/pageParts/PageComponent.vue';
 import { load } from '@/services/eventsService.ts';
 </script>
 
-<template>
-    <PageComponent title="Eventos">
-        <CardItem v-for="item in load()" :key="item?.id" :item="item" relation="events"/>
-    </PageComponent>
-</template>
-
 <style scoped>
-
 </style>
