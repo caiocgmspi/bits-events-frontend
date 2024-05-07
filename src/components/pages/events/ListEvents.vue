d40d037fec323d327147b53b94f7c5d0096c7258
italo <italoguilherme900@gmail.com>
2024-05-06 21:20:11 -0300

    merge

--- src/components/pages/events/ListEvents.vue
+++ src/components/pages/events/ListEvents.vue
@@ -1,7 +1,26 @@
+<template>
+    <PageComponent title="Eventos">
+        <div class="actions">
+            <RouterLink :to="'/events/create'" class="btn btn-primary" style="float: right; margin-top: 50px;">
+                Criar Evento
+            </RouterLink>
+        </div>
+
+        <CardItem v-for="item in data" :key="item?.id" :item="item" relation="events"/>
+    </PageComponent>
+</template>
+
 <script setup>
 import CardItem from '@/components/cardItem.vue';
 import PageComponent from '@/components/pageParts/PageComponent.vue';
 import { load } from '@/services/eventsService.ts';
+import { onMounted, ref } from 'vue';
+
+let data = ref();
+
+onMounted(async () => {
+    data.value = await load();
+})
 </script>
 
 <template>
--- src/services/eventsService.ts
+++ src/services/eventsService.ts
@@ -1,4 +1,4 @@
-export const load = () => {
+export const load = async () => {
     return [
         {
             name: 'Juazeiro Eventos 2024',
@@ -8,18 +8,31 @@ export const load = () => {
             end_in: new Date().toLocaleDateString(),
             participants: '7 de 10',
             id: 5,
+        },
+        {
+            name: 'Bits Digitais 2024',
+            description: 'Uma descriçao qualquer aqui para popular o tamanho de texto!',
+            create_at: new Date().toLocaleDateString(),
+            start_in: new Date().toLocaleDateString(),
+            end_in: new Date().toLocaleDateString(),
+            participants: '7 de 10',
+            id: 2,
         }
     ]
 }
 
-export const create = (form) => {
+export const fetch = async (id) => {
+    return (await load())?.filter((a) => a.id == id)[0] ?? {};
+}
+
+export const create = async (form) => {
 
 }
 
-export const update = (id, form) => {
+export const update = async (id, form) => {
 
 }
 
-export const remove = (id) => {
+export const remove = async (id) => {
 
 }
