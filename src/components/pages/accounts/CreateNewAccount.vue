996d6dfad1667d049d6a7a75debe0b14b725588f
italo <italoguilherme900@gmail.com>
2024-05-18 16:44:07 -0300

    Rota para tela- Criar nova Conta

---
+++ src/components/pages/accounts/CreateNewAccount.vue
@@ -0,0 +1,15 @@
+<script setup>
+import PageComponent from '@/components/pageParts/PageComponent.vue';
+import CreateAccountForm from '@/components/forms/CreateAccountForm.vue';
+
+</script>
+
+<template>
+    <PageComponent title="Criar Nova Conta">
+        <CreateAccountForm />
+    </PageComponent>
+</template>
+
+<style scoped>
+
+</style>
